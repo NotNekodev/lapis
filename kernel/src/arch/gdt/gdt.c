d761c3ed33ab33ae3264f2428b9384b8724433c4
NotNekodev <97458908+NotNekodev@users.noreply.github.com>
2026-07-13 02:02:38 +0200

    better comments asdasdasdsad

--- kernel/src/arch/gdt/gdt.c
+++ kernel/src/arch/gdt/gdt.c
@@ -7,8 +7,8 @@
 // https://github.com/Mathewnd/Astral/blob/rewrite/kernel-src/arch/x86-64/gdt.c#L8
 static uint64_t template[7] = {
 	0, // NULL 0x0
-	0x00af9b000000ffff, // code64 0x8
-	0x00af93000000ffff, // data64 0x10
+	0x00af9b000000ffff, // code64 0xb
+	0x00af93000000ffff, // data64 0x13
 	0x00eff3000000ffff, // udata64 0x1b
 	0x00affb000000ffff, // ucode64 0x20
     0x0020890000000000, // tss low 0x2b
